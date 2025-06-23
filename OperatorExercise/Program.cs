8b131ee893b7e715fb854911d2b9a48f705c5a3f
Addishiwot Ali <sosenamillion@yahoo.com>
2025-06-22 22:45:39 -0700

    finished Operators-Exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,31 @@
 ﻿namespace OperatorExercise
-{
-    public class Program
+{  public class Program
     {
+        public static double AreaCircle(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+
+        public static void printAreaCircle()
+        {
+            Console.WriteLine("what is the radius of your circle?");
+            double radius = double.Parse(Console.ReadLine());
+            Console.WriteLine($"the are of a circle with radius {radius} is {Math.Round(AreaCircle(radius), 3)}");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+            
+            int addition = a + b;
+            int subtraction = a - b;
+            int multiplication = a * b;
+            
+            int division = a / b;
+            int remainder = a % b;
+            Console.WriteLine($"{a}/{b} is {division} remainder {remainder}");
+            
+            printAreaCircle();
         }
     }
 }
